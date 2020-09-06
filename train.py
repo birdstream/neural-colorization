fc972e97e876e59c628f7cf3255fb21833e57c16
Joakim Fogelström <birdstream@hotmail.com>
2020-09-07 01:21:31 +0200

    Update train.py

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ def parse_args():
                         "--training_dir",
                         type=str,
                         required=True,
-                        help="Training directory (folder contains all 256*256 images)")
+                        help="Training directory (folder contains all images)")
     parser.add_argument("-t",
                         "--test_image",
                         type=str,
@@ -89,16 +89,16 @@ def parse_args():
                         type=str,
                         default=None,
                         help="Init weights for generator")
-    parser.add_argument("--d_noise",
-                        type=float,
-                        default=0.01,
-                        help="Discriminator input noise factor")
     parser.add_argument("--d_disable",
                         action="store_true",
                         help="Disable training of discriminator")
     parser.add_argument("--g_disable",
                         action="store_true",
                         help="Disable training of generator")
+    parser.add_argument("--label_fake",
+                        type=float,
+                        default=0.05,
+                        help="Set fake label value (one-sided label smoothing)")
     args = parser.parse_args()
     return args
 
@@ -115,8 +115,9 @@ class img_data(data.Dataset):
         #transform = transforms.RandomAffine(0,(0.2,0.2),(2,1),resample=Image.BICUBIC)
         transform = transforms.Compose([
             #transforms.RandomRotation(20,resample=Image.BICUBIC),
-            transforms.RandomResizedCrop((128,128)),
-            transforms.RandomHorizontalFlip()
+            #transforms.RandomResizedCrop((256,256)),
+            #transforms.RandomHorizontalFlip()
+            transforms.Resize((256,256),interpolation=3)
             ])
         img = transform(img)
         #img = img.filter(ImageFilter.GaussianBlur(np.random.rand()*2))
@@ -186,76 +187,76 @@ plt.show(block=False)
 plt.pause(1)
 
 
-i=0; dls=0
-i_n = args.d_noise
+i=0
 g_loss_gan_history=[]
 g_loss_history=[]
 d_loss_history=[]
-g_onoff=[]
-d_onoff=[]
-d_loss = 0
+d_loss=0
 adversarial_loss = torch.nn.BCELoss()
-optimizer_G = Adam_LRD(G.parameters(), lr=args.g_lr, betas=(0.5, 0.999),dropout=0.5)
-optimizer_D = Adam_LRD(D.parameters(), lr=args.d_lr, betas=(0.5, 0.999),dropout=0.5)
+optimizer_G = Adam_LRD(G.parameters(), lr=args.g_lr, betas=(0.5, 0.999), dropout=0.5)
+#optimizer_D = S(D.parameters(), lr=args.d_lr, betas=(0.5, 0.999),dropout=0.5)
+optimizer_D = Adam_LRD(D.parameters(), lr=args.d_lr, betas=(0.5, 0.999), dropout=0.5)
 for epoch in range(args.epoch):
     for y, uv in training_generator:
         # Adversarial ground truths
-        valid = Variable(torch.Tensor(y.size(0), 1).fill_(1.0), requires_grad=False).cuda(args.gpu)
-        fake = Variable(torch.Tensor(y.size(0), 1).fill_(0.0), requires_grad=False).cuda(args.gpu)
+       # valid = Variable(torch.Tensor(y.size(0), 1).fill_(1.0), requires_grad=False).cuda(args.gpu)
+       # fake = Variable(torch.Tensor(y.size(0), 1).fill_(0.0), requires_grad=False).cuda(args.gpu)
 
-        yvar = Variable(y).cuda(args.gpu)
-        uvvar = Variable(uv).cuda(args.gpu)
-        real_imgs = torch.cat([yvar,uvvar],dim=1)
-        #real_imgs = real_imgs + ((0.1-np.clip(i*0.00001, 0, 0.1))**0.5) * torch.randn(256,256).cuda(args.gpu)
+       # yvar = Variable(y).cuda(args.gpu)
+       # uvvar = Variable(uv).cuda(args.gpu)
+       # real_imgs = torch.cat([yvar,uvvar],dim=1)
 
-        optimizer_G.zero_grad()
-        uvgen = G(yvar)
+
+       # optimizer_G.zero_grad()
+       # uvgen = G(yvar)
         # Generate a batch of images
-        gen_imgs = torch.cat([yvar.detach(),uvgen],dim=1)
+       # gen_imgs = torch.cat([yvar.detach(),uvgen],dim=1)
         #gen_imgs = gen_imgs + ((0.1-np.clip(i*0.00001, 0, 0.1))**0.5) * torch.randn(256,256).cuda(args.gpu)
+        yvar = Variable(y).cuda(args.gpu)
+        uvgen = G(yvar)
+        uvvar = Variable(uv).cuda(args.gpu)
+        gen_imgs = torch.cat([yvar.detach(),uvgen],dim=1)
+        real_imgs = torch.cat([yvar,uvvar],dim=1)
+        valid = Variable(torch.Tensor(y.size(0), 1).fill_(1.0), requires_grad=False).cuda(args.gpu)
+        fake = Variable(torch.Tensor(y.size(0), 1).fill_(args.label_fake), requires_grad=False).cuda(args.gpu)
+        if i%args.g_every==0 and not args.g_disable:
+            for a in range (0,1):
+                optimizer_G.zero_grad()
+                g_loss_gan = adversarial_loss(D(gen_imgs), valid)
+                g_loss = g_loss_gan + args.pixel_loss_weights * torch.mean((uvvar-uvgen)**2)
+                g_loss_gan_history.append(g_loss_gan.item())
+                g_loss_history.append(g_loss.item())
+                g_loss.backward()
+                optimizer_G.step()
+                #flip+=1
+                #if flip == 1000:
+                #    setattr(args, 'g_disable', True)
+                #    setattr(args, 'd_disable', False)
+                #    flip = 0
 
-        # Loss measures generator's ability to fool the discriminator
-        g_loss_gan = adversarial_loss(D(gen_imgs), valid)
-        g_loss = g_loss_gan + args.pixel_loss_weights * torch.mean((uvvar-uvgen)**2)
-        g_loss_gan_history.append(g_loss_gan)
-        g_loss_history.append(g_loss)
-        if i%args.g_every==0 and not args.g_disable and not (g_loss_gan < 0.05 or d_loss > 1.5):
-        #if g_loss_gan.item() > gh:
-            g_loss.backward()
-            optimizer_G.step()
-            g_onoff.append(0)
         else:
-            g_onoff.append(1)
-            g_temp=g_loss.item()
-            g_loss-=g_loss
-            g_loss.backward()
-            optimizer_G.step()
-            g_loss=g_temp
-        #    gh+=.1*gh
-        #else:
-        #    gh-=.1*gh
+            g_loss = 0
+            g_loss_gan = 0
 
-        optimizer_D.zero_grad()
 
-        # Measure discriminator's ability to classify real from generated samples
-        if not args.d_disable and not d_loss < 0.2:
-            real_loss = adversarial_loss(D(real_imgs + i_n**0.5 * torch.randn(128,128).cuda(args.gpu)), valid)
-            fake_loss = adversarial_loss(D((gen_imgs + i_n**0.5 * torch.randn(128,128).cuda(args.gpu)).detach()), fake)
-            d_loss = (real_loss + fake_loss) / 2
-            d_loss.backward()
-            optimizer_D.step()
-            d_onoff.append(0)
-        else:
+        if not args.d_disable:
+            optimizer_D.zero_grad()
+            # Measure discriminator's ability to classify real from generated samples
             real_loss = adversarial_loss(D(real_imgs), valid)
             fake_loss = adversarial_loss(D((gen_imgs).detach()), fake)
             d_loss = (real_loss + fake_loss) / 2
-            d_onoff.append(1)
-            d_temp=d_loss.item()
-            d_loss-=d_loss
+            #d_loss = torch.mean((real_loss + fake_loss)**2)
+            d_loss_history.append(d_loss.item())
             d_loss.backward()
             optimizer_D.step()
-            d_loss=d_temp
-        d_loss_history.append(d_loss)
+            #flip+=1
+            #if flip == 10000:
+            #    setattr(args, 'g_disable', False)
+            #    setattr(args, 'd_disable', True)
+            #    flip = 0
+        else:
+            d_loss = 0
+ 
         if args.test_image is not None and i%args.checkpoint_every==0:
             test_res = G(test_var)
             uv=test_res.cpu().detach().numpy()
@@ -272,32 +273,30 @@ for epoch in range(args.epoch):
                 test_yuv2 = np.concatenate([test_inf2,uv2],axis=1).reshape(3,512,512)
                 test_rgb2 = yuv2rgb(test_yuv2.transpose(1,2,0))
                 im.set_data(np.concatenate([test_rgb.clip(min=0,max=1),test_rgb2.clip(min=0,max=1)]))
-            #plt.draw()
-            #plt.gcf().canvas.draw_idle()
+
             plt.gcf().canvas.start_event_loop(0.001)
             plt.savefig((os.path.join(args.checkpoint_location,'test_epoch_'+str(epoch)+'_iter_'+str(i)+'.jpg')))
 
         i+=1
+
         plt.gcf().canvas.start_event_loop(0.001)
-        print ("Epoch: % 4d: Iter: % 6d [D loss: % 10.5f] [G total loss: % 10.5f] [G GAN loss: % 10.5f] [D Noise: % 1.5f] \r" % (epoch, i, d_loss, g_loss, g_loss_gan, i_n), end='')
+        print ("Epoch: % 4d: Iter: % 6d [D loss: % 10.5f] [G total loss: % 10.5f] [G GAN loss: % 10.5f] \r" % (epoch, i, d_loss, g_loss, g_loss_gan), end='')
         if i%args.checkpoint_every==0:
             print ("\n", end='')
 
             torch.save(D.state_dict(), os.path.join(args.checkpoint_location,'weights','D'+str(epoch)+'.pth'))
             torch.save(G.state_dict(), os.path.join(args.checkpoint_location,'weights','G'+str(epoch)+'.pth'))
             plt.figure('Loss')
-            #plt.xlim(0, len(g_loss_history))
             plt.gcf().clear()
             plt.subplot(311)
             plt.plot(g_loss_history)
             plt.xlabel('Generator g_loss')
+            #plt.ylim(0,100)
             plt.subplot(312)
             plt.plot(g_loss_gan_history)
-            plt.plot(g_onoff)
             plt.xlabel('Generator g_loss_gan')
             plt.subplot(313)
             plt.plot(d_loss_history)
-            plt.plot(d_onoff)
             plt.xlabel('Discriminator d_loss')
             plt.gcf().canvas.draw_idle()
             plt.gcf().canvas.start_event_loop(0.001)
